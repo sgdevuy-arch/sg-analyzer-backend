cdfa6f3e8eb402f6e8c3afb734816d2316657c22
SG Dev <sgdev.uy@gmail.com>
2026-05-26 02:18:49 -0300

    primer backend

---
+++ server.js
@@ -0,0 +1,185 @@
+import express from "express";
+import cors from "cors";
+import puppeteer from "puppeteer";
+
+import lighthouse from "lighthouse";
+import * as chromeLauncher from "chrome-launcher";
+
+const app = express();
+
+app.use(cors());
+app.use("/screenshots", express.static("screenshots"));
+app.use(express.json());
+
+async function analizarSitio(url) {
+
+    const chrome = await chromeLauncher.launch({
+        chromeFlags: ["--headless"]
+    });
+
+    const options = {
+        logLevel: "info",
+        output: "json",
+        port: chrome.port
+        
+    };
+
+    const runnerResult = await lighthouse(url, options);
+
+    const report = runnerResult.lhr;
+
+    //await chrome.kill();
+
+    return {
+
+        performance: Math.round(
+            report.categories.performance.score * 100
+        ),
+
+        seo: Math.round(
+            report.categories.seo.score * 100
+        ),
+
+        accessibility: Math.round(
+            report.categories.accessibility.score * 100
+        ),
+
+        bestPractices: Math.round(
+            report.categories["best-practices"].score * 100
+        )
+    };
+}
+async function tomarScreenshot(url){
+
+    const browser = await puppeteer.launch({
+
+        headless: true
+
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto(url, {
+
+        waitUntil: "networkidle2"
+    });
+
+    const nombreArchivo = `screenshot-${Date.now()}.png`;
+
+    const ruta = `screenshots/${nombreArchivo}`;
+
+    await page.screenshot({
+
+        path: ruta,
+
+        fullPage: true
+    });
+
+    await browser.close();
+
+    return nombreArchivo;
+}
+function generarAnalisis(resultado){
+
+    let recomendaciones = [];
+
+    // PERFORMANCE
+
+    if(resultado.performance < 50){
+
+        recomendaciones.push(
+            "⚡ El rendimiento es bajo. Optimiza imágenes y reduce scripts pesados."
+        );
+
+    } else if(resultado.performance < 80){
+
+        recomendaciones.push(
+            "⚡ El rendimiento es aceptable, pero todavía puede mejorar."
+        );
+
+    } else {
+
+        recomendaciones.push(
+            "⚡ Excelente rendimiento general."
+        );
+    }
+
+    // SEO
+
+    if(resultado.seo < 70){
+
+        recomendaciones.push(
+            "🔍 El SEO necesita mejoras en meta etiquetas y estructura."
+        );
+
+    } else {
+
+        recomendaciones.push(
+            "🔍 Buen nivel de SEO."
+        );
+    }
+
+    // ACCESSIBILITY
+
+    if(resultado.accessibility < 70){
+
+        recomendaciones.push(
+            "♿ Hay problemas de accesibilidad para algunos usuarios."
+        );
+
+    } else {
+
+        recomendaciones.push(
+            "♿ Buena accesibilidad."
+        );
+    }
+
+    // BEST PRACTICES
+
+    if(resultado.bestPractices < 70){
+
+        recomendaciones.push(
+            "✅ Algunas buenas prácticas de seguridad y desarrollo faltan."
+        );
+
+    } else {
+
+        recomendaciones.push(
+            "✅ Buenas prácticas correctas."
+        );
+    }
+
+    return recomendaciones;
+}
+
+app.post("/analizar", async (req, res) => {
+
+    try {
+
+        const { url } = req.body;
+
+        const resultado = await analizarSitio(url);
+        const screenshot = await tomarScreenshot(url);
+
+const recomendaciones = generarAnalisis(resultado);
+
+res.json({
+    ...resultado,
+    recomendaciones,
+    screenshot
+});
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(500).json({
+            error: "Error analizando sitio"
+        });
+    }
+});
+
+app.listen(3000, () => {
+
+    console.log("Servidor funcionando en puerto 3000");
+});
+//node server.js
